ef729aea77d50e9c2193d017a1e9139b3a9d60e5
Metsuge <metsuge@gmail.com>
2023-02-18 22:03:48 +0200

    addons

--- Person.js
+++ Person.js
@@ -14,8 +14,21 @@ class Person extends GameObject {
 		};
 	}
 
+	updatePosition() {
+		if (this.movingProgressRemaining > 0) {
+			// fx it'll select one of the keys from the directionUpdate object
+			//  and return the value which is an array: ["y", -1]
+			// where "property" is the first var. "y", "change" is the second var. -1
+			const [property, change] = this.directionUpdate[this.direction];
+			// variables x or y depending on the direction
+			this[property] += change;
+			this.movingProgressRemaining -= 1;
+		}
+	}
+
 	update(state) {
 		this.updatePosition();
+		this.updateSprite(state);
 		//if it's player controlled game object
 		//if direction key is held AND the previous movement is done
 		if (this.isPlayerControlled && state.arrow && this.movingProgressRemaining === 0) {
@@ -25,15 +38,17 @@ class Person extends GameObject {
 		}
 	}
 
-	updatePosition() {
+	updateSprite(state) {
+		//if there's no movement, just idle standing turned to a direction
+		if (this.isPlayerControlled && !state.arrow && this.movingProgressRemaining === 0) {
+			this.sprite.setAnimation("idle-" + this.direction);
+			return;
+		}
+
+		// if there's movement to a direction
+		//if the movement is not over, do the directions walking animation
 		if (this.movingProgressRemaining > 0) {
-			// fx it'll selec one of the keys from the directionUpdate object
-			//  and return the value which is an array: ["y", -1]
-			// where property is the first var. "y", change is the second var. -1
-			const [property, change] = this.directionUpdate[this.direction];
-			// variables x or y depending on the direction
-			this[property] += change;
-			this.movingProgressRemaining -= 1;
+			this.sprite.setAnimation("walk-" + this.direction);
 		}
 	}
 }
--- Sprite.js
+++ Sprite.js
@@ -2,9 +2,6 @@ class Sprite {
 	constructor(config) {
 		this.gameObject = config.gameObject;
 
-		this.currentAnimation = config.currentAnimation || "idleDown";
-		this.currentAnimationFrame = 0;
-
 		this.image = new Image();
 		this.image.src = config.src;
 		this.image.onload = () => {
@@ -14,21 +11,85 @@ class Sprite {
 		};
 
 		// SHADOW
+		this.useShadow = true; // for now it's default, that all objects use shadow
+
 		this.shadow = new Image();
 		if (this.useShadow) {
 			this.shadow.src = "/images/characters/shadow.png";
 		}
 		this.shadow.onload = () => {
-			this.isShadowLoading = true;
+			this.isShadowLoaded = true;
 		};
-		this.useShadow = true; // for now it's default, that all objects use shadow
 
+		// CONFIGURE ANIMATIONS AND INITIAL MOVING STATE
+
+		this.currentAnimation = "walk-up"; //config.currentAnimation || "idle-down";
+		this.currentAnimationFrame = 0;
 		this.animations = config.animations || {
-			idleDown: [
-				// [x,y] coordinates for the character on the map
+			//coordinatets to get diferent frame from hero.png
+			"idle-down": [[0, 0]],
+			"idle-right": [[0, 1]],
+			"idle-up": [[0, 2]],
+			"idle-left": [[0, 3]],
+			"walk-down": [
+				[1, 0],
+				[0, 0],
+				[3, 0],
 				[0, 0],
 			],
+			"walk-right": [
+				[1, 1],
+				[0, 1],
+				[3, 1],
+				[0, 1],
+			],
+			"walk-up": [
+				[1, 2],
+				[0, 2],
+				[3, 2],
+				[0, 2],
+			],
+			"walk-left": [
+				[1, 3],
+				[0, 3],
+				[3, 3],
+				[0, 3],
+			],
 		};
+
+		this.animationFrameLimit = config.animationFrameLimit || 8; // how many game frames we need to show when object is moving
+		// basically how fast the hero is moving
+		this.animationFrameProgress = this.animationFrameLimit;
+	}
+
+	get frame() {
+		return this.animations[this.currentAnimation][this.currentAnimationFrame];
+	}
+
+	setAnimation(key) {
+		if (this.currentAnimation !== key) {
+			this.currentAnimation = key;
+			this.currentAnimationFrame = 0;
+			this.animationFrameProgress = this.animationFrameLimit;
+		}
+	}
+
+	updateAnimationProgress() {
+		if (this.animationFrameProgress > 0) {
+			//reduce progress by 1 on each frame
+			this.animationFrameProgress -= 1;
+			return;
+		} else {
+			// reset the counter
+			this.animationFrameProgress = this.animationFrameLimit;
+			this.currentAnimationFrame += 1;
+
+			if (this.frame === undefined) {
+				//if there's no more animation frames left in array
+				// loop again from the begining
+				this.currentAnimationFrame = 0;
+			}
+		}
 	}
 
 	draw(ctx) {
@@ -36,20 +97,24 @@ class Sprite {
 		const y = this.gameObject.y - 18;
 
 		//shadow is drawn first because you have to start from the bottom layer
-		// shadow -> character on top of shadow
+		// shadow, then character on top of shadow
+
+		this.isShadowLoaded && ctx.drawImage(this.shadow, x, y);
+
+		const [frameX, frameY] = this.frame;
 
 		this.isLoaded &&
 			ctx.drawImage(
 				this.image,
-				0, //left cut
-				0, //top cut,
+				frameX * 32, //left cut
+				frameY * 32, //top cut,
 				32, //width of cut
 				32, //height of cut
 				x,
 				y,
-				32, // can change shadow position, seem like character is floating,
-				// or the sun direction is changing
+				32, // shadow position
 				32
 			);
+		this.updateAnimationProgress();
 	}
 }
